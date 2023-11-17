9b65c1f2ae95787618892e7d700b1beb56e63c26
benellis3 <benellis@robots.ox.ac.uk>
2023-11-17 14:47:01 +0000

    Small change to run docker

--- run_docker.sh
+++ run_docker.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
-WANDB_API_KEY=$(cat ./setup/wandb_key)
-git pull
+WANDB_API_KEY=$(cat ./setup/docker/wandb_key)
+# git pull
 
 script_and_args="${@:2}"
-gpu=$1
-echo "Launching container diffrl_$gpu on GPU $gpu"
-docker run \
-    --env CUDA_VISIBLE_DEVICES=$gpu \
-    --gpus all \
-    -e WANDB_API_KEY=$WANDB_API_KEY \
-    -v $(pwd):/home/duser/rl-jax-template \
-    --name jaxrl_$gpu \
-    --user $(id -u) \
-    --rm \
-    -d \
-    -t jaxrl \
-    /bin/bash -c "$script_and_args"
+if [ $1 == "all" ]; then
+    gpus="0 1 2 3 4 5 6 7"
+else
+    gpus=$1
+fi
+
+for gpu in $gpus; do
+    echo "Launching container diffrl_$gpu on GPU $gpu"
+    docker run \
+        --gpus device=$gpu \
+        -e WANDB_API_KEY=$WANDB_API_KEY \
+        -v $(pwd):/home/duser/rl-jax-template \
+        --name jaxrl_$gpu \
+        --user $(id -u) \
+        --rm \
+	-d \
+        -t jaxrl \
+        /bin/bash -c "$script_and_args"
+done
