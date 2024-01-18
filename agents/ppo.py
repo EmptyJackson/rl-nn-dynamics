e03c210cefce1fd7eec71a6dfc7735f9e6ac31a6
benellis3 <benellis@robots.ox.ac.uk>
2024-01-18 17:42:26 +0000

    format ppo

--- agents/ppo.py
+++ agents/ppo.py
@@ -10,6 +10,7 @@ def compute_tree_norm(tree):
         sum(jnp.sum(jnp.square(x)) for x in jax.tree_util.tree_leaves(tree))
     )
 
+
 def make_train_step(args, network):
     def _update_step(train_state, aux_train_states, traj_batch, last_obs, rng):
         def _update_epoch(update_state, _):
@@ -96,8 +97,12 @@ def make_train_step(args, network):
                 grads = jax.tree_map(lambda x: x.mean(axis=0), grads)
                 metrics["grad_norm"] = compute_tree_norm(grads)
                 # compute the adam update size
-                metrics["update_norm"] = compute_tree_norm(train_state.tx.update(grads, train_state.opt_state, train_state.params)[0])
-                # compute the similarity between the gradient and the momentum estimates
+                metrics["update_norm"] = compute_tree_norm(
+                    train_state.tx.update(
+                        grads, train_state.opt_state, train_state.params
+                    )[0]
+                )
+                # computemetrics the similarity between the gradient and the momentum estimates
                 train_state = train_state.apply_gradients(grads=grads)
                 return train_state, metrics
 
